ab7d66cd133822f4a42b964c3578bf342215b815
richard-mcghee <richardjmcghee@yahoo.com>
2020-06-19 11:31:47 -0400

    finished closure section

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,21 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
+// Javascript can look outside its current scope to find objects because of the lexical scoping. However, it does not work in the opposite way.
+
+
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function sumation(times){
+  let count = 0;
+  for(let i = 1; i <= times; i++){
+  count += i;
+  }
+  return count;
+}
+
+// console.log(sumation(4));
